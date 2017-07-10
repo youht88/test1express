9a5e865100d22b7ec2bfbe4b1c384f355d7245b8
youht <youht@ylzinfo.com>
2017-07-10 09:47:22 +0800

    modify

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const Mongolass=require('mongolass');
+const mongolass = new Mongolass('mongodb://localhost:27017/local');  
+
+const Users = mongolass.model('users');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Users
+  .find()
+  .select({ name: 1, age: 1 })
+  .sort({ age: 1 })
+  .exec()
+  .then((data)=>{
+    console.log(data);
+    res.render('index', { title: data[1].name})
+  })
+  .catch(console.error);
 });
 
 module.exports = router;
