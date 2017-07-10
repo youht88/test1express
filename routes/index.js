ad9a8057264806922dcf2d49dc42333b77858f91
youht <youht@ylzinfo.com>
2017-07-10 22:18:16 +0800

    aa

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,27 @@ var router = express.Router();
 const Mongolass=require('mongolass');
 const mongolass = new Mongolass('mongodb://localhost:27017/local');  
 
+const cronJob = require('cron').CronJob;
+
 const Users = mongolass.model('users');
+
+let index=0;
+new cronJob('*/30 * * * * *',()=>{
+    index++;
+    Users
+    .insertOne({name:`index${index}`,age:index})
+    .exec()
+    .then(()=>{
+      console.log();
+    })
+},null,true,'Asia/Chongqing');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Users
   .find()
   .select({ name: 1, age: 1 })
-  .sort({ age: 1 })
+  .sort({ age: -1 })
   .exec()
   .then((data)=>{
     console.log(data);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+let request = require('request');
+let cheerio = require('cheerio');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  request('http://www.jikexueyuan.com/',function(error,response,body){
+     if(error && response.statusCode == 200){
+       $ = cheerio.load(body);
+       res.send($)
+     }
+  })
 });
 
 module.exports = router;
