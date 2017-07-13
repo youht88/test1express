8fb7cb2bce9921128da3e6592ba4a55cec9b0c92
youht <youht@ylzinfo.com>
2017-07-13 12:23:52 +0800

    bug

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,12 @@ router.get('/', function(req, res, next) {
   request('http://www.jikexueyuan.com/',function(error,response,body){
      if(!error && response.statusCode == 200){
        $ = cheerio.load(body);
-<<<<<<< HEAD
        res.end($('#BAIDU_DUP_fp_wrapper').length)
      }else{
        res.json({a:1,b:2});
-=======
        res.send(body)
      }else{
        response.end(error);
->>>>>>> 135007f0f9cc7a0d65b9eb262fc0ae9526594c27
      }
 
   })
